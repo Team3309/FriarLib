b2a84095c09ed2a436a44b00b024aa2361abb686
Vinnie Magro <vmagro@socaldevs.com>
2013-07-07 09:13:43 -0700

    MovingAverageFilter: include MovingAverageFilterTest in AllTests.java

--- test/org/team3309/friarlib/test/AllTests.java
+++ test/org/team3309/friarlib/test/AllTests.java
@@ -4,10 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.team3309.friarlib.test.filter.MaxChangeFilterTest;
+import org.team3309.friarlib.test.filter.MovingAverageFilterTest;
 import org.team3309.friarlib.test.util.LatchTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TankDriveTest.class, MaxChangeFilterTest.class, LatchTest.class })
+@SuiteClasses({ TankDriveTest.class, MaxChangeFilterTest.class, LatchTest.class, MovingAverageFilterTest.class })
 public class AllTests {
 
 }
